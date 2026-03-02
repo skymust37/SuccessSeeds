904258bbdb9323510f6019b2bb241c76f5074b7a
skymust37 <skymust@gmail.com>
2026-03-02 10:08:28 +0900

    Initial Commit

---
+++ SuccessSeeds/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SuccessSeeds
+//
+//  Created by 김승현 on 3/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SuccessSeeds/SuccessSeedsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuccessSeedsApp.swift
+//  SuccessSeeds
+//
+//  Created by 김승현 on 3/2/26.
+//
+
+import SwiftUI
+
+@main
+struct SuccessSeedsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
